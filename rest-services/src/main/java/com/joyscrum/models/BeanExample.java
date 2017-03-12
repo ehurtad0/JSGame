7ce97875f7b7c9c1fe7829971210b0349f229429
Jorge Mota <motajorge@hotmail.com>
2017-03-11 22:12:44 -0600

    Initial REST-API Project commit

---
+++ rest-services/src/main/java/com/joyscrum/models/BeanExample.java
@@ -0,0 +1,34 @@
+package com.joyscrum.models;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Created by Jorge Mota
+ * on 3/11/17.
+ */
+@XmlRootElement(name = "BeanExample")
+@XmlAccessorType(XmlAccessType.FIELD)
+
+public class BeanExample {
+    public BeanExample(){
+
+    }
+    private String name;
+    private String who;
+
+    public String getName(){
+        return name;
+    }
+    public String getWho(){
+        return who;
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+    public void setWho(String who){
+        this.who= who;
+    }
+}
---
+++ rest-services/target/rest-services/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+  <h1>Rest Service </h1>
+  <p>Version ${VersionGIT}</a>
+</body>
+</html>
