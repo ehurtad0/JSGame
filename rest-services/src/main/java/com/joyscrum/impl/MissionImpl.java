522c8e8de29ee11fe378595a4f97a4a6e207aeda
Jorge Mota <motajorge@hotmail.com>
2017-04-10 10:54:36 -0600

    changed entities relations

--- rest-services/src/main/java/com/joyscrum/impl/MissionImpl.java
+++ rest-services/src/main/java/com/joyscrum/impl/MissionImpl.java
@@ -2,7 +2,9 @@ package com.joyscrum.impl;
 
 import com.joyscrum.ConnectionDB;
 import com.joyscrum.models.Mission;
+import com.joyscrum.models.MissionPlayer;
 import com.joyscrum.models.Player;
+import com.joyscrum.models.Rol;
 import org.bson.types.ObjectId;
 import org.mongodb.morphia.Datastore;
 
@@ -11,20 +13,59 @@ import javax.ws.rs.NotFoundException;
 import java.util.List;
 
 /**
- * Created by jorge on 4/4/17.
+ * Created by Jorge Mota
+ * on 4/4/17.
  */
 public class MissionImpl {
     @Inject
     ConnectionDB connection;
 
-    public List<Mission> listMissions(ObjectId userId, ObjectId rolId) {
-        if (!ObjectId.isValid(userId.toHexString()) ){
+    public List<Mission> listMissionsByRol(ObjectId userId) {
+        if (!ObjectId.isValid(userId.toHexString())) {
+            throw new NotFoundException("Usuario o Rol no válido");
+        }
+        Datastore store = connection.getDataStore();
+        Player player = store.createQuery(Player.class).field("id").equal(userId).get();
+
+        if (player==null || player.getRol()==null){
+            throw new NotFoundException("Usuario o Rol no válido");
+        }
+        Rol rol = store.createQuery(Rol.class).filter("id",player.getRol().getId()).get();
+        System.out.println(rol.getId().toHexString());
+        List<Mission> missions = store.createQuery(Mission.class).filter("rol._id", rol.getId()).asList();
+
+        return store.createQuery(Mission.class).filter("rol",player.getRol()).asList();//.filter("id",userId).asList();
+        //.asList();
+
+    }
+
+   /* public List<Mission> listMissionsForUser(ObjectId userId, ObjectId rolId) {
+        if (!ObjectId.isValid(userId.toHexString())) {
             throw new NotFoundException("Usuario o Rol no válido");
         }
         Datastore store = connection.getDataStore();
 
-        return  store.createQuery(Mission.class).asList();//.filter("id",userId).asList();
-                //.asList();
+        return store.createQuery(Mission.class).field().equals().asList();//.filter("id",userId).asList();
+        //.asList();
 
+    }*/
+
+    public Player assignMission(ObjectId userId, ObjectId missionId) {
+        Datastore store = connection.getDataStore();
+
+        Player player = store.createQuery(Player.class).field("id").equal(userId).get();
+        if (!ObjectId.isValid(userId.toHexString()) || player == null) {
+            return null;
+        }
+        MissionPlayer mission = store.createQuery(MissionPlayer.class).field("mission._id").equal(missionId).get();
+        if (!ObjectId.isValid(missionId.toHexString()) || mission == null) {
+            return null;
+        }
+
+       // player.setMisionActual(mission);
+        player.setMissionActualId(missionId.toHexString());
+        store.save(player);
+
+        return player;
     }
 }
--- rest-services/src/main/java/com/joyscrum/services/MissionService.java
+++ rest-services/src/main/java/com/joyscrum/services/MissionService.java
@@ -2,12 +2,14 @@ package com.joyscrum.services;
 
 import com.joyscrum.impl.MissionImpl;
 import com.joyscrum.models.Mission;
+import com.joyscrum.models.Player;
 import com.joyscrum.models.ToID;
 import org.bson.types.ObjectId;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.List;
 
 /**
@@ -23,8 +25,20 @@ public class MissionService {
     @GET
     @Path("/{userId}")
     @Produces({MediaType.APPLICATION_JSON})
-    public List<Mission> listPlayers(@PathParam("userId") String  userId, @QueryParam ("rolId")String rolId) {
-        return service.listMissions(new ObjectId(userId),new ObjectId(rolId));
+    public List<Mission> listPlayers(@PathParam("userId") String userId) {
+        return service.listMissionsByRol(new ObjectId(userId));
+    }
+
+    @PUT
+    @Path("/assign/{userId}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response assignMission(@PathParam("userId") String userId, ToID missionId) {
+        Player player = service.assignMission(new ObjectId(userId), new ObjectId(missionId.getHexString()));
+        if (player != null) {
+            return Response.ok().entity(player).build();
+        } else {
+            return Response.status(406).build();
+        }
     }
 
 }
